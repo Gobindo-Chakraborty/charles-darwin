b38392a2bfb532e04fe7d0574902c416dceb3bee
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-05-04 07:58:18 +0600

    React plus Tailwindcss setup added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./pages/HomePage";
+
+function App() {
+  document.documentElement.setAttribute("data-theme", "light");
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import WIP from "../components/WIP";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1 className="text-3xl text-center mt-5">Welcome to Home Page</h1>
+
+      <WIP />
+    </>
+  );
+}
